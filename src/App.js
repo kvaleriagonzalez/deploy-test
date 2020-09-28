442ca6b8c14971e07f4b37a14489d3d3d3e89559
karen <k.valeria.gonzalez@gmail.com>
2020-09-28 11:49:35 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React, { Component } from "react";
+import firebase from "./firebase";
+import "./App.css";
+class App extends Component {
+  render() {
+    return <div className="App"></div>;
+    <div>
+      <h1>hello</h1>
+    </div>
+  }
+}
+export default App;
