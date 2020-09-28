6fa4053c1ed8818668454e846d80b7f9e11a600a
karen <k.valeria.gonzalez@gmail.com>
2020-09-28 12:43:47 -0400

    Updates

---
+++ precache-manifest.f505b75cc178c336c04b76aa96323a20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afdf54538f0338071a6deaa6cd4db0ad",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "3784c0af4cd0d37b348f",
+    "url": "/deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5b720cb6ada735e792b",
+    "url": "/deploy-test/static/js/2.3bbb2c63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-test/static/js/2.3bbb2c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3784c0af4cd0d37b348f",
+    "url": "/deploy-test/static/js/main.4e47d901.chunk.js"
+  },
+  {
+    "revision": "23db0319da0c4e8793b9",
+    "url": "/deploy-test/static/js/runtime-main.d3bbeae5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
